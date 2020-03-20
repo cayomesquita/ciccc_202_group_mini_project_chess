e77f488ec184979e21e51b94c06b27b2f973e227
Tomona Sako <sakotomona@gmail.com>
2020-03-19 23:46:05 -0700

    Fix Pawn

--- src/ca/ciccc/chess/movement/MovementPawnStrategy.java
+++ src/ca/ciccc/chess/movement/MovementPawnStrategy.java
@@ -12,6 +12,7 @@ public final class MovementPawnStrategy extends MovementStrategyAbstract {
 
     private static MovementPawnStrategy instance = new MovementPawnStrategy();
 
+
     private MovementPawnStrategy() {
         super();
     }
@@ -23,15 +24,21 @@ public final class MovementPawnStrategy extends MovementStrategyAbstract {
     @Override
     public List<Movement> getPossibleMoviments(Position originalPosition, Board board, boolean isWhite) {
         List<Movement> list = new ArrayList<>();
-        Position up1 = move(originalPosition, 0, 1);
-        checkNewPosition(originalPosition, board, isWhite, list, up1);
 
-        if (isWhite == true && originalPosition.getRow() == 1) {
-            Position up2 = move(originalPosition, 0, 2);
-            checkNewPosition2(originalPosition, board, isWhite, list, up2);
-        } else if (isWhite == false && originalPosition.getRow() == 6) {
-            Position up2 = move(originalPosition, 0, -2);
-            checkNewPosition2(originalPosition, board, isWhite, list, up2);
+        if(isWhite == true) {
+            Position up1 = move(originalPosition, 0, 1);
+            checkNewPosition(originalPosition, board, isWhite, list, up1);
+            if (originalPosition.getRow() == 1) {
+                Position up2 = move(originalPosition, 0, 2);
+                checkForTwoStep(originalPosition, board, isWhite, list, up2);
+            }
+        } else {
+            Position up1 = move(originalPosition, 0, -1);
+            checkNewPosition(originalPosition, board, isWhite, list, up1);
+            if (originalPosition.getRow() == 6) {
+                Position up2 = move(originalPosition, 0, -2);
+                checkForTwoStep(originalPosition, board, isWhite, list, up2);
+            }
         }
         return list;
     }
@@ -40,28 +47,35 @@ public final class MovementPawnStrategy extends MovementStrategyAbstract {
     protected void checkNewPosition(Position originalPosition, Board board, boolean isWhite, List<Movement> list, Position position) {
         Position frontRight = move(originalPosition, 1, 1);
         Position frontLeft = move(originalPosition, -1, 1);
-        if (isPositionInsideBoard(position, board)) {
-            Piece piece = board.get(position);
+        if(isWhite == false) {
+            frontRight = move(originalPosition, 1, -1);
+            frontLeft = move(originalPosition, -1, -1);
+        }
+            if (isPositionInsideBoard(position, board)) {
+                Piece piece = board.get(position);
                 if (piece == null) {
-                list.add(new Movement(originalPosition, position));
+                    list.add(new Movement(originalPosition, position));
+                }
+            }
+            if (isPositionInsideBoard(frontRight, board)) {
+                Piece piece = board.get(frontRight);
+                if (piece != null && piece.getIsWhite() != isWhite) {
+                    list.add(new Movement(originalPosition, frontRight));
+                }
+            }
+            if (isPositionInsideBoard(frontLeft, board)) {
+                Piece piece = board.get(frontLeft);
+                if (piece != null && piece.getIsWhite() != isWhite) {
+                    list.add(new Movement(originalPosition, frontLeft));
+                }
             }
         }
-        if (isPositionInsideBoard(frontRight, board)) {
-            Piece piece = board.get(frontRight);
-            if (piece != null && piece.getIsWhite() != isWhite) {
-                list.add(new Movement(originalPosition, frontRight));
-            }
-        }
-        if (isPositionInsideBoard(frontLeft, board)) {
-            Piece piece = board.get(frontLeft);
-            if (piece != null && piece.getIsWhite() != isWhite) {
-                list.add(new Movement(originalPosition, frontLeft));
-            }
-        }
-    }
 
-    protected void checkNewPosition2(Position originalPosition, Board board, boolean isWhite, List<Movement> list, Position position) {
+    protected void checkForTwoStep(Position originalPosition, Board board, boolean isWhite, List<Movement> list, Position position) {
         Position front = move(originalPosition, 0, 1);
+        if(isWhite == false) {
+            front = move(originalPosition, 0, -1);
+        }
         if (isPositionInsideBoard(position, board)) {
             Piece piece = board.get(position);
             Piece piece2 = board.get(front);
@@ -69,7 +83,6 @@ public final class MovementPawnStrategy extends MovementStrategyAbstract {
                 list.add(new Movement(originalPosition, position));
             }
         }
-
-
     }
+
 }
